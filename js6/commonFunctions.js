341110113b6cc57a66a3875606814b6e36b9493c
GaRo <gabrielrocheman@gmail.com>
2022-11-18 13:44:05 +0100

    loader pour objets

---
+++ js6/commonFunctions.js
@@ -0,0 +1,18 @@
+/**
+ * loader de vues
+ * @param {string} pageHref filename de la page visée
+ * @param {function} callback function pour l'ajout des eventlisteners
+ * @returns {Promise<Element>} assemble elements loaded in a div element container
+ */
+export function loadPageByPromise(pageHref) {
+    var pagePath=`/vues/${pageHref}`;
+    return fetch(pagePath)
+        .then(function(resp){
+            return resp.text();
+        })
+        .then(function(html) {
+            var container=document.createElement('div');
+            container.innerHTML = html;
+            return container;
+        });
+}
